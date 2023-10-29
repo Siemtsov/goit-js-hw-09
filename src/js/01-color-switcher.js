68242f69116fe302af9a74f974512d142f7cca71
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-29 09:49:38 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
-
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+startButton.addEventListener('click', handlerStart);
+function handlerStart() {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', '');
+}
+stopButton.addEventListener('click', handlerStop);
+function handlerStop() {
+  clearInterval(timer);
+  startButton.removeAttribute('disabled');
+}
