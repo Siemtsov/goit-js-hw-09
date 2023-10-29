ef8907518ad046aed26e862435a441d99341c269
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-29 07:50:02 +0000

    Deploying to gh-pages from @ Siemtsov/goit-js-hw-09@68242f69116fe302af9a74f974512d142f7cca71 🚀

---
+++ 01-color-switcher.276018c2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timer=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(timer),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.276018c2.js.map
