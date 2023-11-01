ba15a26c0309ee6c922053d2a0396b72415bd82d
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-11-01 14:08:26 +0200

    task01 task03

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,7 @@ const stopButton = document.querySelector('[data-stop]');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let timer = null;
 startButton.addEventListener('click', handlerStart);
 function handlerStart() {
   timer = setInterval(() => {
